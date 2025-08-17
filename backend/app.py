1197e1b2ebca96076fd10b80584e88b647c2b282
ROMLIANY <lianirom30@gmail.com>
2025-08-17 17:02:33 +0300

    test

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, request, jsonify
-import mysql.connector
+from flask import Flask, Response
+from prometheus_client import Counter, generate_latest
 
 app = Flask(__name__)
 
-@app.route("/")
+REQUEST_COUNT = Counter('request_count', 'Total number of requests')
+
+@app.route('/')
 def home():
-    return {"message": "Welcome-Home Test!"}
+    REQUEST_COUNT.inc()  
+    return "Hello, Flask is running!"
+
+@app.route('/metrics')
+def metrics():
+    return Response(generate_latest(), mimetype='text/plain')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
